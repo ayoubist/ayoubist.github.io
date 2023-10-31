ae0c4cf0c66f0d7f2dc531211796c2b7a5df53fb
ayoubist <ginaadit1@gmail.com>
2023-10-31 02:58:12 +0100

    .

--- resources/views/partials/errors.blade.php
+++
@@ -1,7 +0,0 @@
-@if(count($errors) > 0)
-@foreach($errors->all() as $error)
-    <div class="alert alert-danger">
-        <li>{!! $error !!}</li>
-    </div>
-@endforeach
-@endif
---
+++ script.js
@@ -0,0 +1,100 @@
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+/* ------------------------------------------------------------------------------------------- */
+
+let count = 0;
+//if add to cart btn clicked
+$('.cart-btn').on('click', function (){
+  let cart = $('.cart-nav');
+  // find the img of that card which button is clicked by user
+  let imgtodrag = $(this).parent('.buttons').parent('.content').parent('.card').find("img").eq(0);
+  if (imgtodrag) {
+    // duplicate the img
+    var imgclone = imgtodrag.clone().offset({
+      top: imgtodrag.offset().top,
+      left: imgtodrag.offset().left
+    }).css({
+      'opacity': '0.8',
+      'position': 'absolute',
+      'height': '150px',
+      'width': '150px',
+      'z-index': '100'
+    }).appendTo($('body')).animate({
+      'top': cart.offset().top + 20,
+      'left': cart.offset().left + 30,
+      'width': 75,
+      'height': 75
+    }, 1000, 'easeInOutExpo');
+
+    setTimeout(function(){
+      count++;
+      $(".cart-nav .item-count").text(count);
+    }, 1500);
+
+    imgclone.animate({
+      'width': 0,
+      'height': 0
+    }, function(){
+      $(this).detach()
+    });
+  }
+});
+
+
+
+
+
+
+
